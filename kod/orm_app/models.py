7c2d6d5b940fcf116dd8d242f2a75a7d5f40c15f
Jakub Lasisz <Kuba@localhost.localdomain>
2014-05-10 16:04:26 +0200

    new model

--- kod/orm_app/models.py
+++ kod/orm_app/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+		
+class Poll(models.Model):
+	question = models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.question
+
 class Choice(models.Model):
 	choice = models.CharField(max_length=200)
 	votes = models.IntegerField()
+	poll = models.ForeignKey(Poll, related_name='choices')
 	
 	def __unicode__(self):
 		return str(self.id) + ' ' + self.choice + ' ' + str(self.votes)
+		
+
