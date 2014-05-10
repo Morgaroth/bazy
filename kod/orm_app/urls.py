5b73f008db4b7d66f65e66d22412080d9bb9049c
Mateusz <mateuszjaje@gmail.com>
2014-05-10 20:21:55 +0200

    widoki do cwiczenia 7 gotowe

--- kod/orm_app/urls.py
+++ kod/orm_app/urls.py
@@ -4,7 +4,10 @@ from orm_app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='choicesindex'),
+    url(r'^users$', views.usersindex, name='usersindex'),
     url(r'^newpoll$', views.createpoll, name='createpoll'),
     url(r'^newchoice$', views.createchoice, name='createchoice'),
+    url(r'^createuser$', views.createuser, name='createuser'),
     url(r'^vote$', views.vote, name='vote'),
+    url(r'^watch$', views.watch, name='watch'),
 )
--- kod/orm_app/views.py
+++ kod/orm_app/views.py
@@ -1,24 +1,45 @@
 from django.shortcuts import render, redirect
-from orm_app.models import Choice, Poll, PollDetails
+from orm_app.models import Choice, Poll, PollDetails, User
 from django.utils import timezone
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 def index(request):
 	polls = Poll.objects.all()
-	print polls[0]
 	return render(request,'polls/index.html',{'polls': polls})
 
 def createchoice(request):
 	Poll.objects.get(pk=request.POST['pollid']).choice_set.create(choice=request.POST['choice'], votes=0)
-	return index(request)	
+	return redirect(reverse('choicesindex'))	
 
 def createpoll(request):
 	poll = Poll(question=request.POST['question'])
 	poll.save()
 	PollDetails(poll=poll,published=timezone.now()).save()
-	return index(request)
+	return redirect(reverse('choicesindex'))	
+	
 
 def vote(request):
 	choice = Choice.objects.get(pk=request.POST['choiceid'])
 	choice.votes+=1
 	choice.save()
-	return index(request)
+	return redirect(reverse('choicesindex'))	
+
+def usersindex(request):
+	users = User.objects.all()
+	return render(request,'users/index.html',{'users': users})
+
+def createuser(request):
+	user = User(name=request.POST['observerName'])
+	user.save()
+	return redirect(reverse('usersindex'))
+
+def watch(request):
+	poll = Poll.objects.get(pk=request.POST['pollid'])
+	users = User.objects.filter(name__exact=request.POST['name'])
+	if(len(users)==0):
+		return HttpResponse("User not exists")
+	else:
+		poll.observers.add(users[0])
+		return redirect(reverse('choicesindex'))
