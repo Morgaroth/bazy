7999b81c9210ab8add12e9499eca3abf17358fc8
Jakub Lasisz <Kuba@localhost.localdomain>
2014-05-10 18:43:45 +0200

    1:1 relation and double join

--- kod/orm_app/models.py
+++ kod/orm_app/models.py
@@ -9,6 +9,14 @@ class Poll(models.Model):
 	def __unicode__(self):
 		return self.question
 
+
+class PollDetails(models.Model):
+	published = models.DateField()
+	poll = models.OneToOneField(Poll)
+	
+	def __unicode__(self):
+		return str(self.published)
+
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice = models.CharField(max_length=200)
